a0c73972b500769065aaec9af5216d13f9151785
gilcot <gildas.cotomale@gmail.com>
2019-11-27 07:39:55 +0100

    initial world release

--- README.md
+++ README.md
@@ -1,2 +1,158 @@
-# tpc
-Teampass API wrapper
+# Teampass API Wrapper
+
+First, let's answer the questions:
+- what is it?
+- how to use it?
+- how does it involve?
+
+After that, you may find some personal thoughts.
+
+## About
+
+This is a POSIX shell script that have very few dependencies.
+
+## Install
+
+First, download the script in your path:
+```shell
+# check it's ok
+echo $PATH
+# e.g. into /usr/local/bin using cURL
+fdest=/usr/local/bin
+fname=tpcli.sh
+curl https//raw.githubcontent.com/gilcot/tapiw/master/$fname -o $fdest/$fname
+```
+Second, make it executable:
+```shell
+chmod a+x $fdest/$fname
+ls -l $fdest/$fname
+```
+Last, create your setup file:
+```shell
+$ cat <<EOF >/etc/tp_cli.rc
+TEAMPASS_URL="https://teampass.example.org"
+TEAMPASS_APIKEY="503E-ca1cE"
+EOF
+```
+
+## Usage
+
+It's first purpose was to ease the use of the API from scripts.  
+In another words, it's a library to use that way:
+```shell
+# define variables used here
+url="https://teampass.example.org"
+key="503E-ca1cE"
+# read fourth entry (login?) of folder 666
+tpcli.sh -u "$url" -k "$key" -c folder -i 666 | cut -f 4
+# read fith (email?) and seventh (pw?) entries of item 9
+tpcli.sh -u "$url" -k "$key" -c item -i 9 | cut -f 5,7
+# find all Bob's entries dealing with sponges (in any field)
+tpcli.sh -u "$url" -k "$key" -c userpw -i bob | grep sponge
+```
+The ease come from the fact that:
+- you don't have to know and retype the API call everytime
+(and, with named instead of positional parameters, you don't
+care the order anymore, at least for reading&hellip;)
+- the response is ready to be consumed with usual commands:
+`sort`, `cut`, `awk`, etc.
+
+The second purpose is to assist the user (interactive mode, use
+the switch `-B` to revert and act like in a script.) by feeding
+a form of needed values. So, previous example becomes:
+```
+$ cat ~/.tp_cli.rc
+TEAMPASS_URL="https://teampass.example.org"
+TEAMPASS_APIKEY="503E-ca1cE"
+$ tpcli.sh -c item
+Item Id?	20
+label	login	url	pw
+webdealauto	nils@yahoo.fr		od0@s'23E
+$ tpcli.sh -c userpw
+User Login?	nilstest
+id	label	description	login	email	url	pw	folder_id	path
+1	DNS server	For all DNS management	Admin	admin@dns.fr	http://mydn.fr	Ud9r^ G7	1	Folder #1
+5	Motorola.com	Motorola customer portal	Jean-Paul	jp.maurice@gmail.com	https://www.motorola.com	Motorola.com	2	F2 - my new folder
+$ tpcli.sh -c userfolders
+User Login?	test
+id	title	level	access_type
+2	My new folder	1	NONE
+5	Sub folder name	2	W
+22	A very long folder name	2	W
+```
+That's handy when adding stuffs (with `-a add` instead of `-a read` which
+is the default.) For example (not final prompts):
+```
+$ tpcli.sh -a add -c item
+Label?	a new test entry
+Password?	fo0b@r
+Description?	for test purpose
+folder Id?	12
+Login?	none
+Email?	jane@doe.com
+Url?
+Tags?
+Any one can modify?	1
+status	new_folder_id
+folder created	56
+```
+
+Deletion (with `-a delete` then) is as easy as reading:
+```
+# from script delete item 7003
+$ tpcli.sh -a delete 7003
+# interactively delete an item
+$ tpcli.sh -a delete
+```
+
+## ChangeLog
+
+Following is the chronological evolution before versionning.
+
+<dl>
+<dt>0.0.0 2019-11-12</dt>
+<dd>I've been playing with our instance without success
+&hellip;till I notice the API wasn't enable. Once done,
+I've able to experiment a little with the API.</dd>
+<dt>0.1.0 2019-11-14</dt>
+<dd>Putting the stuffs into a script to get item to read from
+command line, and parse the returned JSON into tab delimited.</dd>
+<dt>found 2019-11-15</dt>
+<dd>Discovering Vadim Aleksandov worked on a CLI, but can't figure
+out how to use it (see the first issue, it need patching first the
+installed base.)</dd>
+<dt>0.2.0 2019-11-18</dt>
+<dd>New: default to prompting for values and add batch switch</dd>
+<dd>New: add a batch mode option to disable prompting</dd>
+<dt>0.3.0 2019-11-19</dt>
+<dd>New: read almost all known components, after creating a common
+get function.</dd>
+<dd>Now: also add the support of the find method, but I found it a
+bit useless</dd>
+<dd>New: shared function to pass multiple Ids</dd>
+<dt>0.3.1 2019-11-20</dt>
+<dd>Fix: rework on get function to handle errors too.</dd>
+<dt>0.4.0 2019-11-22</dt>
+<dd>New: made a prompt and fail functions and refactorised the code
+(it low the final size while providing a consistant behavieur.) </dd>
+<dt>0.5.0 2019-11-23</dt>
+<dd>New: many synonyms for convenienece, plus both both singular and
+plural forms each time.</dd>
+<dd>New: deletion, addition and update actions now added, after making
+the function able to handle the output (the dedicated part is a bit more
+generic and called for both status and password) and making another one
+to handle base64 encoding (and thus adds another dependancy).</dd>
+<dt>found 2019-11-24</dt>
+<dd>Discovering Jonathan Lestrelin worked on a CLI too, but it's based
+on categories wich are not in use in our installation.</dd>
+<dt>found 2019-11-25</dt>
+<dd>Created this repository in order to share with the world.</dd>
+<dt>0.6.0 2019-11-26</dt>
+<dd>New: shortcut to add/delete folder with components
+`publicfolder` and `userfolder` </dd>
+<dd>New: add categories reading (but not tested.)</dd>
+<dd>New: add undocumented reading folder descendant.</dd>
+<dt>0.6.1 2019-11-26</dt>
+<dd>Fix: after test from an old box, add fallback to CSV</dd>
+
+
---
+++ tpcli.sh
@@ -0,0 +1,481 @@
+#!/bin/sh
+# ex: ai:sw=4:ts=4
+# vim: ai:ft=sh:sw=4:ts=4:ff=unix:sts=4:et:fenc=utf8
+# -*- sh; c-basic-offset: 4; indent-tabs-mode: nil; tab-width: 4; 
+# atom: set UseSoftTabs tabLength=4 encoding=utf-8 lineEnding=lf grammar=shell:
+# mode: shell; tabsoft; tab:4; encoding: utf-8; coding: utf-8;
+###########################################################################
+# Command Line Interface to TeamPass API (see README)
+
+# those are needed by the script
+for _i in awk base64 curl grep jq tty
+do
+    if ! command -v $_i >/dev/null
+    then
+        echo "$_i not in $PATH" >&2
+        exit 1
+    fi
+done
+
+# load paramaters
+if test -r /etc/tp_cli.rc
+then
+    . /etc/tp_cli.rc
+fi
+if test -r $HOME/.tp_cli.rc
+then
+    . $HOME/.tp_cli.rc
+fi
+tp_a='read'
+tp_c='items'
+tp_u="${TEAMPASS_URL:-https://$(hostname)}"
+tp_k="$TEAMPASS_APIKEY"
+tp_o="$CURL_OPTIONS"
+
+# overidde parameters
+while getopts "BHa:c:g:hi:k:m:o:r:s:u:v" arg
+do
+    case $arg in
+        v)
+            echo "tpcli.sh-0.6.1"
+            exit 0
+            ;;
+        h)
+            echo "syntax: $0 [parameter value]... [switch]... Ids..."
+            echo "those parameters may be defined as variable in {/etc,~}/tp_cli.rc"
+            echo " -u: TEAMPASS_URL"
+            echo " -k: TEAMPASS_APIKEY"
+            echo " -o: CURL_OPTIONS"
+            echo "those parameters can only be specified in invocation command line"
+            echo " -a: action (read --default-- or write)"
+            echo " -c: component (items --default, folder, etc.)"
+            echo " -r: restrict search to those folders"
+            echo "switches are used to change default behaviour so"
+            echo " -B: batch mode, i.e. no interactive prompt"
+            echo " -H: don't show headers first"
+            exit 0
+            ;;
+        B) # batch
+            tp_B=1
+            ;;
+        H) # headers
+            tp_H=1
+            ;;
+        a|m) # action|method
+            tp_a="$( echo "$OPTARG" | awk '{print tolower($0)}' )"
+            ;;
+        c) # component
+            tp_c="$( echo "$OPTARG" | awk '{print tolower($0)}' )"
+            ;;
+        i) # id(s)
+            tp_i="$OPTARG"
+            ;;
+        k) # key
+            tp_k="$OPTARG"
+            ;;
+        o) # options for curl
+            tp_o="$OPTARG"
+            ;;
+        r) # restrict
+            tp_r="$OPTARG"
+            ;;
+        s|u) # url|site
+            tp_u="$OPTARG"
+            ;;
+        *)
+            #echo "syntax: $0 [-u|-s <site_url>] [-k <apikey>] [-l <user_login>] [-p <user_password>] [-c <component>] [-B] [-H] [-o <curl_options>] <ids> " >&2
+            exit 1
+            ;;
+    esac
+    shift $(( OPTIND - 1 ))
+    #echo "DEBUG: $OPTIND less, remain $*"
+done
+
+# auto-switch to batch if no term
+tty -s || tp_B=1
+
+# Function to fail with message
+# $1 = optional error message
+# $2 = optional exit code
+_fail() {
+    echo "${1:-Abort}" >&2
+    exit $(( $2 ))
+}
+
+# note: as ${!foo} isn't POSIX, we use _temp
+_temp=''
+# Function to prompt some entry
+# $1 = requiered input variable
+# $2 = requiered prompt message
+# $3 = optional  default value
+# $4 = optional  failure message
+# $5 = optional  if set disable failure on no value
+_prompt() {
+    test $# -lt 2 && return
+    test -z "$_temp" && _temp="$3"
+    if test -z "$tp_B" && test -z "$_temp"
+    then
+        printf "%b?\t" "$2"
+        read -r _temp
+        if test -z "$5"
+        then
+            test -z "$_temp" && _fail "$4" 2
+        fi
+    fi
+    eval "$1='$_temp'"
+    _temp=''
+}
+
+# API endpoint
+if echo "$tp_u" | grep -qsv '/$'
+then
+    tp_u="$tp_u/"
+fi
+if echo "$tp_u" | grep -qs '/index\.php'
+then
+    tp_u="$( echo "$tp_u" |
+        awk '{gsub("/index\.php.*$","/",$0); print $0}' 2>/dev/null )"
+fi
+if echo "$tp_u" | grep -qsv '/api/$'
+then
+    tp_u="${tp_u}api/"
+fi
+tp_u="${tp_u}index.php"
+
+# test instance address
+# output should be: {"err":"Something happen ... but what ?"}
+curl $tp_o -o /dev/null -sS $tp_u ||
+    _fail "Network error or bad API EndPoint: $tp_u" 1
+
+# API access key
+_prompt 'tp_k' "API key" "$tp_k" \
+    "No option -k and no TEAMPASS_APIKEY in config file, but that's required..."
+tp_k="?apikey=$tp_k"
+
+# URL to method call
+tp_u="$tp_u/$tp_a/"
+
+# Function to concatene Multiple Ids List into one argument
+# $* = requiered space separated items
+_mil() {
+    echo "$*" |
+        awk '{out=$1; for(i=2;i<=NF;i++){out=out";"$i}; print out}'
+}
+
+# Function to Encode Data String part according to
+# $1 = requiered string to enconde
+#@ https://teampass.readthedocs.io/en/latest/api/api-write/#information-about-base64_encoding
+# note that spaces are trimed, in order to allow the use of _prompt for empty fields
+_eds() {
+    printf '%s' "$1" |
+        awk '{gsub(/^[ \t]+/,"",$0); gsub(/[ \t]+$/,"",$0); print $0}' |
+        base64 -w 0 |
+        awk '{gsub("+","-",$0); gsub("/","_",$0); print $0}'
+}
+
+# Function to make a simple GET query against the TP API EP
+# $1 = requiered the request as method/component/parameter but key
+_get() {
+    test $# -lt 1 && return
+    #echo "DEBUG: curl $tp_u$1$tp_k"
+    _json=$( curl -s $tp_o -H 'Content-Type: application/json; charset=utf8' "$tp_u$1$tp_k" | grep -s '^{' )
+    #echo "DEBUG: $_json"
+    if jq --version | awk -F '-' '{print $2}' | grep -qs '1\.[1-4]' # jq-X.Y-Z-misc
+    then
+        # 1.5 knows keys_unsorted and @tsv, 1.4 doesn't
+        _out='@csv'
+    else
+        _out='@tsv'
+    fi
+    if echo "$_json" | grep -qs '^{"\(status\|password\)":'
+    then
+        if test -z "$tp_H"
+        then
+            echo "$_json" |
+                jq -r "keys | $_out"
+        fi
+        echo "$_json" |
+            jq -r "[ .[] ] | @tsv"
+    elif echo "$_json" | grep -qs '^{"err":'
+    then
+        test -z "$tp_H" && echo "ErRoR"
+        echo "$_json" | jq -r '.err'
+    else
+        if test -z "$tp_H"
+        then
+            echo "$_json" |
+                jq -r "[ .[] ] | .[0] | keys | $_out"
+        fi
+        echo "$_json" |
+            jq -r "(.[] | [ .[] ]) | [ .[] ] | $_out"
+    fi
+}
+
+# labels/question shown
+l_tf=" boolean integer:\n 0 false\n 1 true\n"
+l_g0="Folder Id"
+l_g1=" folder title (public)\n user login (personal)\nFolder Name"
+l_g2=" integer in:\n 00 very weak,\n 25 weak,\n 50 mean,\n 60 strong,\n 70 very strong,\n 80 sure,\n 90 very sure\nComplexity Lvl"
+l_g3=" boolean integer:\n 0 public\n 1 personal\nFolder Type"
+l_g4=" days integer, 0 for unlimited\nRenewal Period"
+l_g5=" 0 for root level\nParent Id"
+l_g8="Folder Title"
+l_g9="Folder(s) Id(s)"
+l_i0="Item Id"
+l_i1="Unique Label"
+l_i2="Password"
+l_i3="Description"
+l_i5="Login"
+l_i6="E-mail"
+l_i7="Web Address"
+l_i8=" space delimited words\nTags List"
+l_i4="${l_tf}Any one can modify"
+l_i9="Item(s) Id(s)"
+l_u0="User Login"
+
+# dispatch per actions and components
+case $tp_a in
+    new_password)
+    #@ https://teampass.readthedocs.io/en/latest/api/api-special/#generate-a-password
+        _prompt 'tp_1' " integer taken from 4 to 50\nSize/Length" "$1"
+        _prompt 'tp_2' "${l_tf}is Secure" "$2"
+        _prompt 'tp_3' "${l_tf}has Numerals" "$3"
+        _prompt 'tp_4' "${l_tf}has Capitals" "$4"
+        _prompt 'tp_5' "${l_tf}has Ambiguous" "$5"
+        _prompt 'tp_6' "${l_tf}has Symbols" "$6"
+        _prompt 'tp_7' "${l_tf} mandatory with symbols\nBase64 Enc." "$7"
+        _get "$(( tp_1 ));$(( tp_2 ));$(( tp_3 ));$(( tp_4 % 2 ));$(( tp_5 % 2 ));$(( tp_6 % 2 ));$(( tp_6?1:tp_7 % 2 ))"
+        ;;
+    auth)
+    #@ https://teampass.readthedocs.io/en/latest/api/api-special/#authentication-credentials-for-a-web-page
+        _prompt 'tp_1' " (i.e. http|https|ftp|ftps|etc.)\nProtocol" "$1"
+        _prompt 'tp_2' " (i.e. without the protocol and /)\nBase URL" "$2" '' 'AllowEmpty'
+        _prompt 'tp_3' "Your Login" "${TEAMPASS_LOGIN:-$3}"
+        _prompt 'tp_4' "Pass Word" "${TEAMPASS_PASSWORD:-$4}"
+        _get "$tp_1/$tp_2/$tp_3/$tp_4"
+        ;;
+    find)
+    #@ https://teampass.readthedocs.io/en/latest/api/api-read/#find-items
+        _prompt 'tp_r' "$l_g9" "$1"
+        _prompt 'tp_i' "Search Pattern" "$2"
+        _get "item/$( _mil "$tp_r" )/$tp_i"
+        ;;
+    read)
+    #@ https://teampass.readthedocs.io/en/latest/api/api-read/#overview
+        case $tp_c in
+            folder|folders|group|groups)
+            #@ https://teampass.readthedocs.io/en/latest/api/api-read/#read-folders
+                _prompt 'tp_i' "$l_g9" "$@"
+                # https://github.com/nilsteampassnet/TeamPass/issues/2307
+                _get "folder/$( _mil "$tp_i" )"
+            ;;
+            entry|entries|item|items|pw)
+            #@ https://teampass.readthedocs.io/en/latest/api/api-read/#read-specific-items
+                _prompt 'tp_i' "$l_i9" "$@"
+                _get "items/$( _mil $tp_i )"
+            ;;
+            userpw|useritems|userfolders)
+            #@ https://teampass.readthedocs.io/en/latest/api/api-read/#read-users-items
+            #@ https://teampass.readthedocs.io/en/latest/api/api-read/#read-users-folders
+                test "$tp_c" = 'useritems' && tp_c='userpw'
+                _prompt 'tp_i' "$l_u0" "$1"
+                _get "$tp_c/$( echo "$tp_i" | awk '{print $1}' )"
+            ;;
+            listattachments|listfiles)
+            #@ https://teampass.readthedocs.io/en/latest/api/api-read/#list-itemss-file-attachments
+                _prompt 'tp_i' "$l_i0" "$1"
+                _get "listfiles/$( echo "$tp_i" | awk '{print $1}' )"
+            ;;
+            files|file|attachment|getfile|getattachment|retrievefile|retrieveattachment)
+            #@ https://teampass.readthedocs.io/en/latest/api/api-read/#download-itemss-file-attachments
+                tp_c='files'
+                _prompt 'tp_i' "File Id" "$1"
+                curl -s $tp_o "$tp_u$tp_c/$( echo "$tp_i" | awk '{print $1}' )$tp_k" -o "teampass_attachment_$tp_i"
+            ;;
+            folder_descendants|folders_descendants|folders_recursive|subfolders)
+            #@ https://teampass.userecho.com/communities/1/topics/106-search-item-by-label-via-api-without-folderid
+            #@ https://teampass.userecho.com/communities/1/topics/85-api-list-folder-by-label-or-list-all-folders
+                _prompt 'tp_r' "$l_g0" "$1"
+                tp_r="$( echo "$tp_r" | awk '{print tolower($1)}' )"
+                if "tp_r" = 'title'
+                then
+                    #_prompt 'tp_i' "$l_g1" "$2"
+                    _prompt 'tp_i' "$l_g8" "$2"
+                elif "tp_r" = 'id'
+                then
+                    _prompt 'tp_i' "$l_g0" "$2"
+                else
+                    _fail "Bad Answer..."
+                fi
+                _get "$tp_c/$tp_r/$( echo "$tp_i" | awk '{print $1}' )"
+            ;;
+            category)
+            #@ https://github.com/jle64/teampassclient
+                _prompt 'tp_r' "Category Id" "$1"
+                _prompt 'tp_i' "Search Label" "$2"
+                _get "$tp_c/$( echo "$tp_r" | awk '{print $1}' )/$tp_i"
+            ;;
+            *)
+            #@ https://teampass.readthedocs.io/en/latest/api/api-read/#
+                curl -s $tp_o "$tp_u$tp_c/$( _mil "${tp_i:-$@}" )$tp_k" |
+                    grep '^{' | jq -r '.'
+            ;;
+        esac
+        ;;
+    info)
+    #@ https://teampass.readthedocs.io/en/latest/api/api-special
+        # https://github.com/nilsteampassnet/TeamPass/issues/1665
+            _fail "Action/Method not handled yet: $tp_a" 0
+        ;;
+    delete)
+    #@ https://teampass.readthedocs.io/en/latest/api/api-write
+        case $tp_c in
+            folder|folders|group|groups)
+            #@ https://teampass.readthedocs.io/en/latest/api/api-write/#delete-a-folder
+                _prompt 'tp_i' "$l_g9" "$@"
+                # https://github.com/nilsteampassnet/TeamPass/issues/2307
+                _get "folder/$( _mil "$tp_i" )"
+            ;;
+            entry|entries|item|items|pw)
+            #@ https://teampass.readthedocs.io/en/latest/api/api-write/#delete-an-item
+                _prompt 'tp_i' "$l_i9" "$@"
+                _get "item/$( _mil $tp_i )"
+            ;;
+            *)
+            #@ https://teampass.readthedocs.io/en/latest/api/api-write/#
+                curl -s $tp_o "$tp_u$tp_c/$( _mil "${tp_i:-$@}" )$tp_k" |
+                    grep '^{' | jq -r '.'
+            ;;
+        esac
+        ;;
+    add)
+    #@ https://teampass.readthedocs.io/en/latest/api/api-write
+        case $tp_c in
+            folder|group)
+            #@ https://teampass.readthedocs.io/en/latest/api/api-write/#add-a-folder
+                _prompt 'tp_1' "$l_g1" "$1"
+                _prompt 'tp_2' "$l_g2" "$2"
+                _prompt 'tp_5' "$l_g5" "$5"
+                _prompt 'tp_3' "$l_g3" "$3"
+                _prompt 'tp_4' "$l_g4" "$4"
+                #_get "folder/$( _eds "$tp_1" );$( _eds $(( tp_2 )) );$( _eds $(( tp_5 )) );$( _eds $(( tp_4 )) );$( _eds $(( tp_3 % 2 )) )"
+                _get "folder/$( _eds "$tp_1;$(( tp_2 ));$(( tp_5 ));$(( tp_4 ));$(( tp_3 % 2 ))" )"
+            ;;
+            publicfolder)
+            #@ https://teampass.readthedocs.io/en/latest/api/api-write/#add-a-folder
+                _prompt 'tp_1' "$l_g8" "$1"
+                _prompt 'tp_2' "$l_g2" "$2"
+                _prompt 'tp_5' "$l_g5" "$4"
+                _prompt 'tp_4' "$l_g4" "$3"
+                #_get "folder/$( _eds "$tp_1" );$( _eds $(( tp_2 )) );$( _eds $(( tp_5 )) );$( _eds $(( tp_4 )) );$( _eds 0 )"
+                _get "folder/$( _eds "$tp_1;$(( tp_2 ));$(( tp_5 ));$(( tp_4 ));0" )"
+            ;;
+            userfolder)
+            #@ https://teampass.readthedocs.io/en/latest/api/api-write/#add-a-folder
+                _prompt 'tp_1' "$l_u0" "$1"
+                _prompt 'tp_2' "$l_g2" "$2"
+                _prompt 'tp_5' "$l_g5" "$4"
+                _prompt 'tp_4' "$l_g4" "$3"
+                #_get "folder/$( _eds "$tp_1" );$( _eds $(( tp_2 )) );$( _eds $(( tp_5 )) );$( _eds $(( tp_4 )) );$( _eds 1 )"
+                _get "folder/$( _eds "$tp_1;$(( tp_2 ));$(( tp_5 ));$(( tp_4 ));1" )"
+            ;;
+            entry|item|pw)
+            #@ https://teampass.readthedocs.io/en/latest/api/api-write/#add-an-item
+                _prompt 'tp_1' "$l_i1" "$1"
+                _prompt 'tp_2' "$l_i2" "$2"
+                _prompt 'tp_3' "$l_i3" "$3" '' 'AllowEmpty'
+                _prompt 'tp_4' "$l_g0" "$4" #
+                _prompt 'tp_5' "$l_i5" "$5" '' 'AllowEmpty'
+                _prompt 'tp_6' "$l_i6" "$6" '' 'AllowEmpty'
+                _prompt 'tp_7' "$l_i7" "$7" '' 'AllowEmpty'
+                _prompt 'tp_8' "$l_i8" "$8" '' 'AllowEmpty'
+                _prompt 'tp_9' "$l_i4" "$9" #
+                _get "item/$( _eds "$tp_1" );$( _eds "$tp_2" );$( _eds "$tp_3" );$( _eds $(( tp_4 )) );$( _eds "$tp_5" );$( _eds "$tp_6" );$( _eds "$tp_7" );$( _eds "$tp_8" );$( _eds $(( tp_9 % 2)) )"
+            ;;
+            user)
+            #@ https://teampass.readthedocs.io/en/latest/api/api-special/#add-new-user
+                _prompt 'tp_1' "$l_u0" "$1"
+                _prompt 'tp_2' "User First Name" "$2"
+                _prompt 'tp_3' "User Last Name" "$3"
+                _prompt 'tp_4' "User Password" "$4"
+                _prompt 'tp_5' "${l_tf}is AdministratedBy" "$5" '' 'AllowEmpty'
+                _prompt 'tp_6' "${l_tf}is Read Only" "$6" '' 'AllowEmpty'
+                _prompt 'tp_7' "${l_tf}is Administrator" "$7" '' 'AllowEmpty'
+                _prompt 'tp_8' "${l_tf}is Users Manager" "$8" '' 'AllowEmpty'
+                _prompt 'tp_9' "${l_tf}has Own Folder" "$9" '' 'AllowEmpty'
+                #_get "user/$( _eds "$tp_1" );$( _eds "$tp_2" );$( _eds "$tp_3" );$( _eds "$tp_4" );$( _eds $(( tp_5 % 2 )) );$( _eds $(( tp_6 % 2 )) );$( _eds $(( tp_7 % 2 )) );$( _eds $(( tp_8 % 2 )) );$( _eds $(( tp_9 % 2 )) )"
+                _get "user/$( _eds "$tp_1;$tp_2;$tp_3;$tp_4;$(( tp_5 % 2 ));$(( tp_6 % 2 ));$(( tp_7 % 2 ));$(( tp_8 % 2 ));$(( tp_9 % 2 ))" )"
+            ;;
+            attachment|file)
+            #@ https://teampass.readthedocs.io/en/latest/api/api-write/#add-new-file-attachment
+                _prompt 'tp_i' "$l_i0" "$2"
+                _prompt 'tp_r' "File Path" "$1"
+                curl -X POST -s $tp_o "${tp_u}file$tp_k" \
+                    -F "item_id=$(( tp_i ))" \
+                    -F "file=@$tp_r;filename=$( basename "$tp_i" )"
+            ;;
+            *)
+            #@ 
+                _fail "Unmanaged component for $tp_a: $tp_c" 2
+            ;;
+        esac
+        ;;
+    update)
+    #@ https://teampass.readthedocs.io/en/latest/api/api-write
+        case $tp_c in
+            entry|item|pw)
+            #@ https://teampass.readthedocs.io/en/latest/api/api-write/#update-an-item
+                _prompt 'tp_0' "$l_i0" "$1"
+                _prompt 'tp_1' "$l_i1" "$2"
+                _prompt 'tp_2' "$l_i2" "$3"
+                _prompt 'tp_3' "$l_i3" "$4"
+                _prompt 'tp_4' "$l_g0" "$5" #
+                _prompt 'tp_5' "$l_i5" "$6" '' 'AllowEmpty'
+                _prompt 'tp_6' "$l_i6" "$7" '' 'AllowEmpty'
+                _prompt 'tp_7' "$l_i7" "$8" '' 'AllowEmpty'
+                _prompt 'tp_8' "$l_i8" "$9" '' 'AllowEmpty'
+                _prompt 'tp_9' "$l_i4" "${10}" #
+                _get "item/$tp_0/$( _eds "$tp_1" );$( _eds "$tp_2" );$( _eds "$tp_3" );$( _eds $(( tp_4 )) );$( _eds "$tp_5" );$( _eds "$tp_6" );$( _eds "$tp_7" );$( _eds "$tp_8" );$( _eds $(( tp_9 % 2)) )"
+            ;;
+            folder|group)
+            #@ https://teampass.readthedocs.io/en/latest/api/api-write/#add-a-folder
+                _prompt 'tp_0' "$l_g0" "$1"
+                _prompt 'tp_1' "$l_g1" "$2"
+                _prompt 'tp_2' "$l_g2" "$3"
+                _prompt 'tp_5' "$l_g5" "$6"
+                _prompt 'tp_3' "$l_g3" "$4"
+                _prompt 'tp_4' "$l_g4" "$5"
+                #_get "folder/$(( tp_0 ))/$( _eds "$tp_1" );$( _eds $(( tp_2 )) );$( _eds $(( tp_5 )) );$( _eds $(( tp_4 )) );$( _eds $(( tp_3 % 2 )) )"
+                _get "folder/$(( tp_0 ))/$( _eds "$tp_1;$(( tp_2 ));$(( tp_5 ));$(( tp_4 ));$(( tp_3 % 2 ))" )"
+            ;;
+            publicfolder)
+            #@ https://teampass.readthedocs.io/en/latest/api/api-write/#add-a-folder
+                _prompt 'tp_0' "$l_g0" "$1"
+                _prompt 'tp_1' "$l_g8" "$2"
+                _prompt 'tp_2' "$l_g2" "$3"
+                _prompt 'tp_5' "$l_g5" "$5"
+                _prompt 'tp_4' "$l_g4" "$4"
+                #_get "folder/$(( tp_0 ))/$( _eds "$tp_1" );$( _eds $(( tp_2 )) );$( _eds $(( tp_5 )) );$( _eds $(( tp_4 )) );$( _eds 0 )"
+                _get "folder/$(( tp_0 ))/$( _eds "$tp_1;$(( tp_2 ));$(( tp_5 ));$(( tp_4 ));0" )"
+            ;;
+            userfolder)
+            #@ https://teampass.readthedocs.io/en/latest/api/api-write/#add-a-folder
+                _prompt 'tp_0' "$l_g0" "$1"
+                _prompt 'tp_1' "$l_u0" "$2"
+                _prompt 'tp_2' "$l_g2" "$3"
+                _prompt 'tp_5' "$l_g5" "$5"
+                _prompt 'tp_4' "$l_g4" "$4"
+                #_get "folder/$(( tp_0 ))/$( _eds "$tp_1" );$( _eds $(( tp_2 )) );$( _eds $(( tp_5 )) );$( _eds $(( tp_4 )) );$( _eds 1 )"
+                _get "folder/$(( tp_0 ))/$( _eds "$tp_1;$(( tp_2 ));$(( tp_5 ));$(( tp_4 ));1" )"
+            ;;
+            *)
+            #@ 
+                _fail "Unmanaged component for $tp_a: $tp_c" 2
+            ;;
+        esac
+        ;;
+    *)
+    #@
+        _fail "Action/Method not handled yet: $tp_a" 2
+        ;;
+esac
